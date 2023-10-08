492ed102b9e44bcf2712edf08c0d80c1e2a2fde9
patrickpt <p.schnass@gmail.com>
2023-10-08 08:21:16 +0200

    Add comments and config file

--- rag_chatbot.py
+++ rag_chatbot.py
@@ -3,37 +3,57 @@ from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
+import yaml
 
-st.set_page_config(page_title="Chatbot", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+with open("config.yaml", "r") as yamlfile:
+    config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+# import configuration from yaml
+name = config[0]['config']['name']
+info = config[0]['config']['info']
+system_prompt = config[0]['config']['system_prompt']
+api = config[0]['config']['api']
+
+# Set Streamlit page configuration
+st.set_page_config(page_title=name, page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+# Set OpenAI API key
 openai.api_key = st.secrets.openai_key
-st.title("llama_index Knowledge Bot")
-st.info("This bot knows everything about PromptEngineering which is mentioned in https://www.promptingguide.ai/", icon="📃")
-         
+
+# Create main interface
+st.title(name)
+st.info(info, icon="📃")
+
+# Initialize the chat messages history         
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question"}
     ]
 
-@st.cache_resource(show_spinner=False)
+# Function to load data
+@st.cache_resource(show_spinner=False) # data is cached in memory so limit the knowledge base according to your machine
 def load_data():
     with st.spinner(text="Loading and indexing the provided data"):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on Prompt Engineering and Retrieval Augmented Generation with Large Language Models. Assume that all questions are related to Prompt Engineering. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True) # read recursively all directories 
+        docs = reader.load_data() # load data and create docs
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model=api, temperature=0.5, system_prompt=system_prompt))# add a permanent service prompt which is added
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context) # create your vector database
         return index
 
+# Load data and create the chat engine
 index = load_data()
 chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
+# User input and chat history
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
+# Display chat history
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# If last message is not from assistant, generate a new response
+# Generate a response if the last message is not from the assistant
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
